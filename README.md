8735ef7a9c75dd155fc8d3f5a01a2a31e10536bd
Beck <apolloheliosethos@gmail.com>
2026-01-14 22:15:29 -0600

    A: Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sudoku in Pygame
+This project is an implementation of Sudoku using Pygame. 
+
+- Package Manager : UV 
+- Python Version: 3.13
---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+
+def main():
+    """Main Sudoku Game Loop"""
+    print("Hello from sudoku!")
+    
+
+
+if __name__ == "__main__":
+    main()
